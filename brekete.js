7c76e87215a360956fd32438c39999cf28140ea9
korosihoss <korosihossana771@gmail.com>
2024-10-14 11:08:28 +0100

    random changes

--- brekete.js
+++ brekete.js
@@ -1,5 +1,5 @@
 function checkInput() {
-    let userInput = document.getElementById("inputField").value;
+    let userInput = 300;//document.getElementById("inputField").value;
     userInput = parseInt(userInput);
     if(!isNaN(userInput)){
         return checkAndParseEligibility(userInput);
@@ -8,22 +8,35 @@ function checkInput() {
     }
 }
 
+
+checkInput()
+
 function checkAndParseEligibility(rechargeAmnt) {    
     if(rechargeAmnt >= 100){
         //parse eligible amount by using modulos
-        let bonusEligAmnt = rechargeAmnt - (rechargeAmnt%100);
-        return bonuses(bonusEligAmnt);
+            let bonusEligAmnt = rechargeAmnt - (rechargeAmnt%100);
+            calculateMainTalkTime(rechargeAmnt);
+            return giveBonuses(bonusEligAmnt);
+        }
+   else{
+            return calculateMainTalkTime(rechargeAmnt);
         }    
 }
 
-function bonuses(bonusEligible){
+function giveBonuses(bonusEligible){
     let airtimeBonus = bonusEligible*4;
     let dataBonus = (airtimeBonus*0.1);
-    return calculateTalkTime(airtimeBonus);
+    console.log(airtimeBonus, dataBonus)
+    return calculateBonusTalkTime(airtimeBonus);
 }
 
-function calculateTalkTime(airtimeBonus) {
-     let bonusTalkTimeInSec = (0.77*airtimeBonus);
+function calculateBonusTalkTime(airtimeBonus) {
+     let bonusTalkTimeInSec = (airtimeBonus/0.83);
      let bonusTalkTime = Math.trunc(bonusTalkTimeInSec/60)+ ":" +Math.trunc(bonusTalkTimeInSec/60%1*60);
-     return bonusTalkTime;
+}
+
+function calculateMainTalkTime(airtime) {
+     let talkTimeInSec = (airtime/0.36);
+     let talkTime = Math.trunc(talkTimeInSec/60)+ ":" +Math.trunc(talkTimeInSec/60%1*60);//find reminder
+     console.log(airtime);
 }
